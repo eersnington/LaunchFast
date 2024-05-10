a8decb03075b1414365f20585986247e7077f963
Sree Narayanan <sreeaadhi07@gmail.com>
2024-05-10 13:38:52 +0530

    Update site-footer.tsx

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -70,7 +70,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              Popsy
+              Popsy.
             </Link>
           </p>
 
