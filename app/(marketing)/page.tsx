71197d7f3d7f9094c4f58067f6e06c9fa95371a8
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-08 21:11:14 +0400

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,14 +4,14 @@ import { Features } from "@/components/sections/features";
 import { HeroLanding } from "@/components/sections/hero-landing";
 import { InfoLanding } from "@/components/sections/info-landing";
 import { Powered } from "@/components/sections/powered";
-import { PreviewLanding } from "@/components/sections/preview-landing";
 import { Testimonials } from "@/components/sections/testimonials";
+import { HeroScrollDemo } from "@/components/sections/preview-landing";
 
 export default async function IndexPage() {
   return (
     <>
       <HeroLanding />
-      <PreviewLanding />
+      <HeroScrollDemo />
       <Powered />
       <BentoGrid />
       <InfoLanding data={infos[0]} reverse={true} />
