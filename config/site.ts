5649ba0bd9e5bfad81573f2ad6463be27484f8e8
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-08 20:07:59 +0400

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,11 +3,12 @@ import { env } from "@/env.mjs";
 
 const site_url = env.NEXT_PUBLIC_APP_URL;
 
+
 export const siteConfig: SiteConfig = {
-  name: "Domain Dash",
+  name: "LaunchFast",
   author: "Sreenington",
   description:
-    "Never worry about domain renewals again. Domain Dash is a domain management tool that helps you keep track of all your domains in one place.",
+    "Don't waste your time building your next Startup from scratch! LaunchFast is powered by Next.js 14, Prisma, Supabase, Auth.js v5, Resend, React Email, Shadcn/ui, and Stripe. Ready for production and type-safety, it's your launchpad to build with confidence.",
   url: site_url,
   ogImage: `${site_url}/og.jpg`,
   links: {
@@ -15,28 +16,43 @@ export const siteConfig: SiteConfig = {
     github: "-",
   },
   twitterID: "@sreenington",
-  mailSupport: "sree@zaplineai.cloud",
+  mailSupport: "sree.zaplineai@gmail.com",
   seoKeywords: [
-    "Domain Management",
-    "Domain Renewal",
-    "Domain Expiry",
-    "Domain Dash",
-    "Domain Manager",
-    "Domain Monitoring",
-    "Domain Alerts",
-    "Domain Notifications",
-    "Domain Tracking",
-    "Domain Expiry Date",
+    "Next.js",
+    "React",
+    "Prisma",
+    "Neon",
+    "Auth.js",
+    "shadcn ui",
+    "Resend",
+    "React Email",
+    "Stripe",
+    "Next.js SaaS",
+    "React SaaS",
+    "Prisma SaaS",
+    "Supabase integration",
+    "Auth.js security",
+    "Shadcn UI design",
+    "Resend email service",
+    "React Email templates",
+    "Stripe payments",
+    "SaaS development tools"
   ],
-  defaultTheme: "light" // "dark" | "light" | "system"
+  defaultTheme: "dark" // "dark" | "light" | "system"
 };
 
 export const footerLinks: SidebarNavItem[] = [
   {
-    title: "Domain Dash",
+    title: "Launch Fast",
     items: [
-      { title: "features", href: "#" },
-      { title: "Pricing", href: "/pricing" },],
+      { title: "About", href: "#" },
+      { title: "Pricing", href: "/pricing" },
+      { title: "Blog", href: "/blog" },
+      { title: "Documentation", href: "/docs" },
+      // { title: "Enterprise", href: "#" },
+      // { title: "Partners", href: "#" },
+      // { title: "Jobs", href: "#" },
+    ],
   },
   {
     title: "Socials",
@@ -51,7 +67,7 @@ export const footerLinks: SidebarNavItem[] = [
     items: [
       { title: "Terms of Service", href: "/terms" },
       { title: "Privacy Policy", href: "/privacy" },
-      // { title: "Refund Policy", href: "#" },
+      { title: "License", href: "/license" },
     ],
   },
 ];
